7fc8acb95cdc2053fccf37755b9c1173164923ba
ISS Operator <xf08id@xf08id-ws01.cs.nsls2.local>
2019-01-25 08:22:35 -0500

    Dan gives up.

--- processor.py
+++ processor.py
@@ -1,11 +1,10 @@
 import time
 import copy
+from collections import namedtuple
 import numpy
 import logging
 
 logger = logging.getLogger('iss_processor')
-logging.basicConfig(level='DEBUG')
-
 
 from databroker import Broker
 from ophyd.sim import NumpySeqHandler
@@ -15,10 +14,63 @@ processed = Broker.named('temp')  # makes a second, unique temporary Broker
 # PROCESSING
 
 import sys
-from event_model import Filler, compose_run, DocumentRouter, pack_event_page
+import xas.interpolate
+from event_model import Filler, compose_run, DocumentRouter, pack_event_page, verify_filled
 from bluesky.callbacks.zmq import RemoteDispatcher, Publisher
 
 publisher = Publisher('localhost:5577', prefix=b'interpolated')
+
+
+# HANDLERS COPIED FROM PROFILE -- MOVE THESE TO CENTRAL PLACE!!
+
+class PizzaBoxEncHandlerTxt:
+    encoder_row = namedtuple('encoder_row',
+                             ['ts_s', 'ts_ns', 'encoder', 'index', 'state'])
+    "Read PizzaBox text files using info from filestore."
+    def __init__(self, fpath, chunk_size):
+        self.chunk_size = chunk_size
+        with open(fpath, 'r') as f:
+            self.lines = list(f)
+
+    def __call__(self, chunk_num):
+        cs = self.chunk_size
+        return [self.encoder_row(*(int(v) for v in ln.split()))
+                for ln in self.lines[chunk_num*cs:(chunk_num+1)*cs]]
+
+
+class PizzaBoxDIHandlerTxt:
+    di_row = namedtuple('di_row', ['ts_s', 'ts_ns', 'encoder', 'index', 'di'])
+    "Read PizzaBox text files using info from filestore."
+    def __init__(self, fpath, chunk_size):
+        self.chunk_size = chunk_size
+        with open(fpath, 'r') as f:
+            self.lines = list(f)
+
+    def __call__(self, chunk_num):
+        cs = self.chunk_size
+        return [self.di_row(*(int(v) for v in ln.split()))
+                for ln in self.lines[chunk_num*cs:(chunk_num+1)*cs]]
+
+
+class PizzaBoxAnHandlerTxt:
+    encoder_row = namedtuple('encoder_row', ['ts_s', 'ts_ns', 'index', 'adc'])
+    "Read PizzaBox text files using info from filestore."
+
+    bases = (10, 10, 10, 16)
+    def __init__(self, fpath, chunk_size):
+        self.chunk_size = chunk_size
+        with open(fpath, 'r') as f:
+            self.lines = list(f)
+
+    def __call__(self, chunk_num):
+
+        cs = self.chunk_size
+        return [self.encoder_row(*(int(v, base=b) for v, b in zip(ln.split(), self.bases)))
+                for ln in self.lines[chunk_num*cs:(chunk_num+1)*cs]]
+
+handler_registry = {'PIZZABOX_AN_FILE_TXT': PizzaBoxAnHandlerTxt,
+                    'PIZZABOX_ENC_FILE_TXT': PizzaBoxEncHandlerTxt,
+                    'PIZZABOX_DI_FILE_TXT': PizzaBoxDIHandlerTxt}
 
 def is_applicable(start_doc):
     ...
@@ -30,20 +82,22 @@ def my_analysis_function(arr, factor):
     return factor * numpy.sum(arr)
 
 
-class Processor(DocumentRouter):
-    version = 1
+class Interpolator(Filler):
+    version = xas.__version__
     
-    def __init__(self, factor):
-        self.factor = factor
+    def __call__(self, name, doc):
+        _, processed_doc = super().__call__(name, doc)
 
     def start(self, doc):
+        doc = super().start(doc)
         metadata = {'raw_uid': doc['uid'],
                     'processor_version': self.version,
-                    'processor_parameters': {'factor': self.factor}}
+                    'processor_parameters': {}}
         self.compose_run_bundle = compose_run(metadata=metadata)
         return self.compose_run_bundle.start_doc
 
     def descriptor(self, doc):
+        doc = super().descriptor(doc)
         name = 'primary'
         data_keys = {'sum': {'shape': [], 'dtype': 'number', 'source': repr(self)}}
         self.compose_descriptor_bundle = self.compose_run_bundle.compose_descriptor(
@@ -52,46 +106,25 @@ class Processor(DocumentRouter):
         return self.compose_descriptor_bundle.descriptor_doc
         
     def event_page(self, doc):
-        result = [my_analysis_function(img, self.factor) for img in doc['data']['img']]
-        event_doc = self.compose_descriptor_bundle.compose_event_page(
-                data={'sum': result},
-                timestamps={'sum': [time.time()] * len(result)},
-                seq_num=doc['seq_num'],
-                validate=False)  # FIXME
-        return event_doc
+        doc = super().event_page(doc)
+        verify_filled(doc)
+        key, = doc['data']
+        print('event_page doc', doc)
+        result = [xas.interpolate.interpolate(dataframe) for dataframe in doc['data'][key]]
+        event_page = self.compose_descriptor_bundle.compose_event_page(
+            data={'sum': result},
+            timestamps={'sum': [time.time()] * len(result)},
+            seq_num=doc['seq_num'],
+            validate=False)  # FIXME
+        return event_page
 
     def stop(self, doc):
+        doc = super().stop(doc)
         return self.compose_run_bundle.compose_stop()
-
-
-class LiveProcessor(DocumentRouter):
-    def __init__(self, factor):
-        self.factor = factor
-
-    def __call__(self, name, doc):
-        if name == 'start':
-            return self.start(doc)
-        if not self.applicable:
-            return
-        _, filled_doc = self.filler(name, doc)
-        _, processed_doc = self.processor(name, filled_doc)
-        print(processed_doc)
-        processed_doc.pop('id', None)
-        processed.insert(name, processed_doc)
-        publisher(name, processed_doc)
-
-    def start(self, doc):
-        self.filler = Filler({"NPY_SEQ": NumpySeqHandler})
-        self.applicable = is_applicable(doc)
-        self.processor = Processor(factor=self.factor)
-        _, processed_doc = self.processor('start', doc)
-        processed.insert('start', processed_doc)
-        publisher('start', processed_doc)
-
 
 
 dispatcher = RemoteDispatcher('localhost:5578', prefix=b'raw')
 
 if __name__ == '__main__':
-    dispatcher.subscribe(LiveProcessor(factor=float(sys.argv[1])))
+    dispatcher.subscribe(Interpolator(handler_registry))
     dispatcher.start()
